cdcb1a7098ba0ee0cd87f60cf4551fa5bac4b250
RGROHIT9699 <119799265+RGROHIT9699@users.noreply.github.com>
2024-09-14 23:44:17 +0530

    main

---
+++ 3rd year/carousel/app.js
@@ -0,0 +1,17 @@
+
+let arr = [
+    'https://images.unsplash.com/photo-1725610588086-b9e38da987f7?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1725610588142-b2b0a03c29d4?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1725610588095-f117c0e2a921?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1725663656846-ffb1041257d3?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1725663656832-416a3954af6a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+]
+
+let i1 = document.querySelector('img')
+
+let n = 0;
+setInterval(()=>{
+    i1.setAttribute('src',arr[n]);
+    i1.style.width= "60%"
+    n = (n+1)%arr.length;
+},3000)
